57a5266525cc90e699b53141fd02c6c33d59bcd7
ziqiu-wang <2236531605@qq.com>
2024-11-20 14:48:30 -0500

    Updated style for linting

--- machine-learning-client/app.py
+++ machine-learning-client/app.py
@@ -5,7 +5,7 @@ This module includes functions to download or load the model, make inferences
 with the model, and parse the inference result and output the model's prediction.
 """
 
-from pymongo import MongoClient
+# from pymongo import MongoClient
 from transformers import pipeline
 
 
@@ -17,7 +17,8 @@ def download_model():
     Returns:
         None
     """
-    pipe = pipeline("audio-classification", model="leo-kwan/wav2vec2-base-100k-gtzan-music-genres-finetuned-gtzan")
+    pipe = pipeline("audio-classification", \
+                    model="leo-kwan/wav2vec2-base-100k-gtzan-music-genres-finetuned-gtzan")
     pipe.save_pretrained("model")
 
 
@@ -65,7 +66,7 @@ def main(audio_file):
     result = []
     try:
         result = inference(audio_file)
-    except:
+    except Exception as _:
         download_model()
         result = inference(audio_file)
     pred = parse_result(result)
